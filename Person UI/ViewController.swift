030423a13f02948c340287288c42c90aa8561b21
Brendan Krekeler <krekeler@mwc-136238.dhcp.missouri.edu>
2018-06-20 12:37:49 -0500

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Brendan Krekeler on 6/20/18.
+//  Copyright © 2018 Brendan Krekeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
